b656265e69e710662b5878d86d4b11715f195ab4
Christina Deming <christinamdeming@gmail.com>
2020-07-27 11:58:16 -0400

    Seperated the countdown from the rest of the changes so they aren't constantly ticking, too.  Added a check so the timer doesn't go past zero, and if it ever gets to zero run the main functions again.  Added some nice styling, too.

--- howl/howl.js
+++ howl/howl.js
@@ -32,32 +32,47 @@ let deadline;
 let moonName;
 let moonPic;
 
-// And also it needs to count, I think this will be okay since the variables will be set by the time it runs
+// And also it needs to visibly count, it's only ever set by running the countdown and is cleared when it shouldn't be ticking
+// moving the image display out of the countdown wouldn't be a bad idea.
+
 
 let tick;
 
 // Set countdown timer and do the math and make variables for the math
 
 function countdown() { 
-    let now = new Date().getTime();
-    let time = deadline - now;
-    let days = Math.floor(time / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let mins = Math.floor((time % (1000 * 60 * 60 )) / (1000 * 60));
-    let secs = Math.floor((time % (1000 * 60 )) / 1000);
-    // Display countdown timer block
-    document.getElementById("demo").innerHTML = `
-        ${days} D ${hours} H ${mins} M ${secs} S
-        <br>
-        until it's time to howl at the
-        <br>
-        ${moonName} Moon
-        <p><img src="images/${moonPic}" alt="${moonName} Moon"></p>`
-    //repeat as needed
-    tick = setInterval(countdown, 1000);
+    var now = new Date().getTime();
+    var time = deadline - now;
+    if (time > 0) { // Let's not go past 0
+        let days = Math.floor(time / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor((time % (1000 * 60 * 60 )) / (1000 * 60));
+        let secs = Math.floor((time % (1000 * 60 )) / 1000);
+        // Display countdown timer block, minimum digits 2 so it's not bouncing all over the place
+        document.getElementById("counter").innerHTML = `
+            ${days.toLocaleString(undefined,{minimumIntegerDigits: 2})}D 
+            ${hours.toLocaleString(undefined,{minimumIntegerDigits: 2})}H 
+            ${mins.toLocaleString(undefined,{minimumIntegerDigits: 2})}M 
+            ${secs.toLocaleString(undefined,{minimumIntegerDigits: 2})}S`
+        //repeat as needed
+        tick = setInterval(countdown, 1000);
+    }
+    else { // A countdown has obviously completed!  nextMoon should NOT catch anything, but checkSameDay SHOULD
+        nextMoon();
+        checkSameDay();
+    }
 }
-// We should determine the upcoming moon, which we can do using epoch time:
-// Ideally we would be making a ticking timer but not yet, I just need to make the idea work first
+
+function whatMoon() {
+    document.getElementById("moon").innerHTML += `
+        <br>
+        until it's time to <span class="howl">howl</span> at the
+        <br>
+        <span class="name">${moonName} Moon</span>`
+    document.body.style.backgroundImage = `url("images/${moonPic}")`
+}
+
+// We should determine the upcoming moon, which we can do using unix time
 // I have the feeling I could do something with an array and a loop but fuck it, long hand way still works
 
 function nextMoon() {
@@ -66,78 +81,91 @@ function nextMoon() {
         moonName = wolfMoon.name;
         moonPic = wolfMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < snowMoon.date.getTime() && currentDay > wolfMoon.date.getTime()) {
         deadline = snowMoon.date.getTime();
         moonName = snowMoon.name;
         moonPic = snowMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < crowMoon.date.getTime() && currentDay > snowMoon.date.getTime()) {
         deadline = crowMoon.date.getTime();
         moonName = crowMoon.name;
         moonPic = crowMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < grassMoon.date.getTime() && currentDay > crowMoon.date.getTime()) {
         deadline = grassMoon.date.getTime();
         moonName = grassMoon.name;
         moonPic = grassMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < flowerMoon.date.getTime() && currentDay > grassMoon.date.getTime()) {
         deadline = flowerMoon.date.getTime();
         moonName = flowerMoon.name;
         moonPic = flowerMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < strawberryMoon.date.getTime() && currentDay > flowerMoon.date.getTime()) {
         deadline = strawberryMoon.date.getTime();
         moonName = strawberryMoon.name;
         moonPic = strawberryMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < thunderMoon.date.getTime() && currentDay > strawberryMoon.date.getTime()) {
         deadline = thunderMoon.date.getTime();
         moonName = thunderMoon.name;
         moonPic = thunderMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < sturgeonMoon.date.getTime() && currentDay > thunderMoon.date.getTime()) {
         deadline = sturgeonMoon.date.getTime();
         moonName = sturgeonMoon.name;
         moonPic = sturgeonMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < redMoon.date.getTime() && currentDay > sturgeonMoon.date.getTime()) {
         deadline = redMoon.date.getTime();
         moonName = redMoon.name;
         moonPic = redMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < harvestMoon.date.getTime() && currentDay > redMoon.date.getTime()) {
         deadline = harvestMoon.date.getTime();
         moonName = harvestMoon.name;
         moonPic = harvestMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < blueMoon.date && currentDay > harvestMoon.date) {
         deadline = blueMoon.date;
         moonName = blueMoon.name;
         moonPic = blueMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < frostMoon.date && currentDay > blueMoon.date) {
         deadline = frostMoon.date;
         moonName = frostMoon.name;
         moonPic = frostMoon.pic;
         countdown();
+        whatMoon();
     }
     else if (currentDay < longMoon.date && currentDay > frostMoon.date) {
         deadline = longMoon.date;
         moonName = longMoon.name;
         moonPic = longMoon.pic;
         countdown();
+        whatMoon();
     }
     else {
         // In case I never get back to this
@@ -150,10 +178,9 @@ function nextMoon() {
 
 function isSameDay() {
     document.getElementById("demo").innerHTML = `
-    Start howling, friends, it's
+    Start <span class="howl">howling</span> friends, it's the
     <br>
-    ${moonName} Moon
-    <p><img src="images/${moonPic}" alt="${moonName} Moon"></p>`;
+    <span class="name">${moonName} Moon</span>`;
     tick = "";
 }
 
